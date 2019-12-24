b5197b256491f73675bb098f1c7cca9dabcedd7c
yaogengzhu <455947455@qq.com>
2019-12-24 09:21:09 +0800

    fix

--- src/view/main/main.tsx
+++ src/view/main/main.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Header from './components/header/header'
 import Slider from './components/slider/slider'
-class Main extends React.Component {
-    
+import { Switch, Route, RouteComponentProps, Redirect } from 'react-router-dom'
+
+interface IProps {
+
+}
+class Main extends React.Component<IProps & RouteComponentProps, {}> {
+
+    public componentDidMount() {
+        console.log(this.props)
+    }
 
     public render() {
         return(
@@ -10,7 +18,13 @@ class Main extends React.Component {
                 <Header></Header>
                 <div className='row'>
                     <Slider></Slider>
-                    <div className='expaned'></div>
+                    <div className='expaned'>
+                        <Switch>
+                            <Route path={ this.props.match.path + '/index'}></Route>
+                            {/* 重定向 */}
+                            <Redirect to={ this.props.match.path + '/index'}></Redirect>
+                        </Switch>
+                    </div>
                 </div>
             </div>
         )
